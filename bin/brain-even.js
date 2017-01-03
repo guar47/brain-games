19dc5ca5e5c7a56794571aeb4571a218f8a117e0
guar47 <guar47@ya.ru>
2017-01-03 16:36:09 +0000

    update app

--- bin/brain-even.js
+++ bin/brain-even.js
@@ -1,4 +1,8 @@
 #!/usr/bin/env node
  // @flow
+import readlineSync from 'readline-sync';
 
 console.log('Welcome to the Brain Games!');
+
+const actual = readlineSync.question('Your answer: ');
+console.log(`Hello ${actual}`);
